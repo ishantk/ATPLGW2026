5ec65faa59e69a885395fed7c4fc64ce5ad689ce
ishantk <er.ishant@gmail.com>
2026-06-30 12:07:47 +0530

    Session5

---
+++ Session5G.py
@@ -0,0 +1,18 @@
+"""
+    BINGO: Flat 100 off, min amount 200
+    GET500: Flat 500 off, min amount 100
+    JUMBO: Flat 300 off, min amount 500
+
+"""
+
+amount_in_cart = int(input('Enter Amount: '))
+promo_code = input('Enter Promo Code: ')
+
+if promo_code == 'BINGO':
+    pass
+elif promo_code == 'GET500':
+    pass
+elif promo_code == 'JUMBO':
+    pass
+else:
+    print('Invalid Promo Code')
---
+++ Session5H.py
@@ -0,0 +1,22 @@
+"""
+    Another Brick in the Wall
+
+    customer - 13 bricks
+    iteration 1     3
+    john  1         
+    jack  2
+    iteration 2     6+3 = 9
+    john  2
+    jack  4
+    iteration 3     9 + 3 + 1 => 13 
+    john  3
+    jack  6 -> 1
+
+
+    Answer: Who placed the last brick and how many
+    jack placed the last brick. qty: 1
+
+"""
+
+# CONTROLLER
+# for/if/else/operators
