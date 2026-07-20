d15fc0ee44981b8076d3ad755354c69bb9400421
ishantk <er.ishant@gmail.com>
2026-07-20 11:29:36 +0530

    Session18

--- Session18B.py
+++ Session18B.py
@@ -1,3 +1,51 @@
 import streamlit as st
+import pandas as pd
+
 st.title('Home Page')
-st.markdown('Welcome to the Home Page')
+# st.markdown('Welcome to the Home Page')
+st.write('Welcome to the Home Page')
+st.divider()
+
+col1, col2, col3, col4 = st.columns(4)
+col1.metric('Patients', 326)
+col2.metric('Doctors', 21)
+col3.metric('Appointments', 54)
+col4.metric('Revenue', '12.5 Lakhs')
+
+st.divider()
+
+left, right = st.columns([2, 1])
+
+with left:
+    st.subheader('Monthly Patients')
+
+    chart = pd.DataFrame({
+        'Month': ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+        'Patients': [120, 140, 180, 170, 210, 250]
+    })
+
+    st.line_chart(
+        chart.set_index('Month')
+    )
+
+
+
+with right:
+    st.subheader('Departments')
+
+    departments = pd.DataFrame({
+        'Department': [
+            'Cardiology',
+            'Dental',
+            'Neuro',
+            'Ortho'
+        ],
+        'Doctors':[5, 4, 7, 8]
+    })
+
+    st.dataframe(
+        departments,
+        use_container_width=True
+    )
+
+st.divider()
