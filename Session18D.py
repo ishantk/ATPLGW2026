e4745e25966d8d05d09fede59b09b0926df7cdc2
ishantk <er.ishant@gmail.com>
2026-07-20 11:41:20 +0530

    Session18

--- Session18D.py
+++ Session18D.py
@@ -1,3 +1,22 @@
 import streamlit as st
+import pandas as pd
+
 st.title('All Patients')
 st.markdown('Welcome to All Patients Page')
+
+patients = {
+    'ID': [101, 102, 103, 104, 105],
+    'NAME': ['John', 'Jim', 'Jack', 'Joe', 'Fionna'],
+    'AGE': [34, 23, 56, 77, 31],
+    'GENDER': ['male', 'male', 'male', 'male', 'female'],
+    'DISEASE': ['Diabetes', 'BP', 'Asthma', 'BP', 'Diabetes'],
+    'DOCTOR': ['Dr Rahul ', 'Dr Anna', 'Dr George', 'Dr Anna', 'Dr Rahul']
+}
+
+patients_data = pd.DataFrame(patients)
+# print(patients_data)
+
+st.dataframe(
+    patients_data,
+    use_container_width=True
+)
